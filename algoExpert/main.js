b068b8d811d11a7800a0be6523e5f1555017a88d
Abdelrhman98 <abdelrhman.soleman98@gmail.com>
2022-01-04 10:01:18 +0200

    leet code

--- algoExpert/main.js
+++ algoExpert/main.js
@@ -1,14 +1,22 @@
-function nonConstructibleChange(coins) {
-    coins.sort((a,b)=>{return a-b})
-    var coinSum = 0
-    coins.forEach(element => {
-        if(coinSum+1 < element)return coinSum+1
-        coinSum+=element
-    });
-    return coinSum+1;
-}
 
-console.log(nonConstructibleChange([5,7,1,1,2,3,22]))
+//https://leetcode.com/problems/group-anagrams/submissions/
+var groupAnagrams = function(strs) {
+    var map ={},
+    result  = []
+    strs.forEach(ele=>{
+        let temp = ele.split("").sort().join("")
+        if(map[temp]){
+            map[temp].push(ele)
+        }else{
+            map[temp] =[ele]
+        }
+    })
+    for(const key in map){
+        result.push(map[key])
+    }
+    return result
+};
+console.log(groupAnagrams( ["eat","tea","tan","ate","nat","bat"]))
 
 // 1 1 3  5   7   22
 // 1 2 5 10  17   
