7b29ed6f0abdf7a7d292abfe0d73324b6d6f2bcf
Abdelrhman98 <abdelrhman.soleman98@gmail.com>
2022-01-11 15:01:42 +0200

    agood day

--- algoExpert/main.js
+++ algoExpert/main.js
@@ -33,7 +33,104 @@ var reverse = function(x) {
     
 }
 
+function getNthFib(n) {
+    if([0,1,2].includes(n))
+          return n
+      return getNthFib(n-1) +getNthFib(n-2)
+  }
 
-console.log(reverse(1534236469))
-// 1 1 3  5   7   22
+//https://www.algoexpert.io/questions/Find%20Three%20Largest%20Numbers
+function findThreeLargestNumbers(array) {
+    
+    var mx = [-Infinity,-Infinity,-Infinity]
+    
+    for(let i = 0;i<array.length;i++){
+        if(array[i] >= mx[2]){
+            mx[0] = mx[1]
+            mx[1] = mx[2]
+            mx[2] = array[i]
+        }
+        else if(array[i] >= mx[1]){
+            mx[0] = mx[1]
+            mx[1] = array[i]
+        }else if(array[i] >= mx[0]){
+            mx[0] = array[i]
+        }
+    }
+    return mx;
+}
+
+//https://www.algoexpert.io/questions/Three%20Number%20Sum
+function threeNumberSum(array, targetSum) {
+    var freq={}, res=[]
+    var visited = {}
+    array.forEach(ele=>{freq[ele]=true})
+    for(let i = 0;i<array.length;i++){
+        for(let j=i+1;j<array.length;j++){
+            if(i!=j)
+            {
+                let third = targetSum-(array[i]+array[j])
+                var arr = [array[i], array[j], third]
+                var srtd = arr.sort((a,b)=>{return a-b}),
+                str = `${srtd}`
+                if(visited[str] == undefined){
+                    if( freq[third]  && third!= array[i] && third!=array[j]){
+                        visited[str] = true
+                        res.push(srtd)
+                    }
+                }
+            }
+            
+        }
+    }
+
+    res.sort((a,b)=>{
+        if(a[0] == b[0])
+            return a[1]-b[1]
+        return a[0] - b[0]
+        
+    })
+    return res
+}
+
+function runLengthEncoding(string) {
+    var allChars = string.split(''),
+    genString="",
+    activeChar="",
+    cnt=0,
+    last=allChars.length,
+    idx=1
+    
+    allChars.forEach(ele=>{
+        if(ele == activeChar)
+            cnt+=1
+        
+        if(ele!=activeChar || idx == last){
+            genString+= ("9"+activeChar).repeat( (cnt/9).toFixed(0) ) +
+                        (cnt-( (cnt/9).toFixed(0) * 9 ))+activeChar
+            activeChar=ele
+            cnt=1
+        }
+        if(idx == last){
+            activeChar=ele
+            genString+= ("9"+activeChar).repeat( (cnt/9).toFixed(0) ) +
+                        (cnt-( (cnt/9).toFixed(0) * 9 ))+activeChar
+        }
+        idx++
+    })
+    let last2 = genString.length
+    var sub = genString.substring(genString.length-4).split('')
+    
+    
+    if(sub[1]==sub[3] && sub[0]!='9')
+        last2 -=2
+    else 
+        last2 = last2
+    //if()
+    
+    return genString.substring(1,last2)
+}
+
+console.log(runLengthEncoding( "AAAAAAAAAAAAABBCCCCDD"))
+// 1 1 3  5   7   22a
 // 1 2 5 10  17   
