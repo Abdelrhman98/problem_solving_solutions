b174353a9e499db6d10cbae03987e74fa3c90029
Abdelrahman Soliman <a.soliman@egyptsmartcards.com>
2022-02-02 10:46:21 +0200

    bubble sort

--- algoExpert/easy.js
+++ algoExpert/easy.js
@@ -48,3 +48,19 @@ function nonConstructibleChange(coins) {
     });
     return coinSum+1;
 }
+
+
+//https://www.algoexpert.io/questions/Bubble%20Sort
+function bubbleSort(array) {
+    for(let i=0; i<array.length - 1 ; i++){
+        for(let j=0;j<array.length-1-i;j++){
+            if(array[j]>array[j+1]){
+                let temp  =  array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp
+            }
+        }
+    }
+    return array
+}
+console.log(bubbleSort([8, 5, 2, 9, 5, 6, 3]))
